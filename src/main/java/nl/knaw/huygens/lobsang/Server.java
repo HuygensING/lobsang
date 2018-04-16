021d469695fb673bdffe9a1a13c6310caa908f85
HDJ <hayco.de.jong@huygens.knaw.nl>
2018-04-16 14:36:30 +0200

    Expose git+build props via manifest

--- src/main/java/nl/knaw/huygens/lobsang/Server.java
+++ src/main/java/nl/knaw/huygens/lobsang/Server.java
@@ -1,6 +1,7 @@
 package nl.knaw.huygens.lobsang;
 
 import io.dropwizard.Application;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import nl.knaw.huygens.lobsang.config.LobsangConfig;
@@ -10,12 +11,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import java.util.logging.Level;
 
 import static org.glassfish.jersey.logging.LoggingFeature.Verbosity.PAYLOAD_ANY;
 
 public class Server extends Application<LobsangConfig> {
   private static final Logger LOG = LoggerFactory.getLogger(Server.class);
+
+  private static String findVersionInfo(String applicationName) throws IOException {
+    Enumeration<URL> resources = Thread.currentThread().getContextClassLoader()
+                                       .getResources("META-INF/MANIFEST.MF");
+    while (resources.hasMoreElements()) {
+      URL manifestUrl = resources.nextElement();
+      Manifest manifest = new Manifest(manifestUrl.openStream());
+      Attributes mainAttributes = manifest.getMainAttributes();
+      String implementationTitle = mainAttributes.getValue("Implementation-Title");
+      LOG.debug("implementationTitle: {}", implementationTitle);
+      if (implementationTitle != null && implementationTitle.equals(applicationName)) {
+        String implementationVersion = mainAttributes.getValue("Implementation-Version");
+        String buildTime = mainAttributes.getValue("Build-Time");
+        return implementationVersion + " (" + buildTime + ")";
+      }
+    }
+    return "Current Version";
+  }
 
   public static void main(String[] args) throws Exception {
     new Server().run(args);
@@ -31,9 +55,14 @@ public class Server extends Application<LobsangConfig> {
     LOG.info("initializing");
   }
 
-  public void run(LobsangConfig lobsangConfig, Environment environment) {
+  public void run(LobsangConfig lobsangConfig, Environment environment) throws IOException {
     setupLogging(environment);
-    environment.jersey().register(new AboutResource());
+    registerResources(environment.jersey());
+    LOG.debug("version: {}", findVersionInfo(getName()));
+  }
+
+  private void registerResources(JerseyEnvironment jersey) {
+    jersey.register(new AboutResource());
   }
 
   private void setupLogging(Environment environment) {
