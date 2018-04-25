f42a285cafe7acd534de5124f130a956d84ccbf0
HDJ <hayco.de.jong@huygens.knaw.nl>
2018-04-25 14:06:56 +0200

    Use fixture for empty manifest test

--- src/test/java/nl/knaw/huygens/lobsang/LobsangIntegrationTest.java
+++ src/test/java/nl/knaw/huygens/lobsang/LobsangIntegrationTest.java
@@ -1,16 +1,21 @@
 package nl.knaw.huygens.lobsang;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectReader;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import nl.knaw.huygens.lobsang.config.LobsangConfig;
+import org.assertj.core.api.Assertions;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.ws.rs.client.Client;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 class LobsangIntegrationTest {
@@ -20,10 +25,14 @@ class LobsangIntegrationTest {
   );
 
   @Test
-  void runServerTest() {
-    Client client = new JerseyClientBuilder().build();
-    String result = client.target(
-      String.format("http://localhost:%d/about", DROPWIZARD.getLocalPort())).request().get(String.class);
-    assertThat(result).contains("Manifest");
+  void runServerTest() throws IOException {
+    final Client client = new JerseyClientBuilder().build();
+    final ObjectReader reader = DROPWIZARD.getObjectMapper().reader();
+
+    final JsonNode actual = reader.readTree(client.target(
+      String.format("http://localhost:%d/about", DROPWIZARD.getLocalPort())).request().get(String.class));
+    final JsonNode expected = reader.readTree(fixture("fixtures/empty_manifest.json"));
+
+    Assertions.assertThat(actual).isEqualTo(expected);
   }
 }
