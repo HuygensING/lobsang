3feb36731e913d7cbbae64b2330facf805801792
HDJ <hayco.de.jong@huygens.knaw.nl>
2018-04-25 14:47:45 +0200

    Use ParameterResolver to inject test manifest

--- src/test/java/nl/knaw/huygens/lobsang/resources/AboutEndpointTest.java
+++ src/test/java/nl/knaw/huygens/lobsang/resources/AboutEndpointTest.java
@@ -4,36 +4,23 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectReader;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import io.dropwizard.testing.junit5.ResourceExtension;
-import org.junit.jupiter.api.BeforeAll;
+import nl.knaw.huygens.lobsang.ManifestParameterResolver;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.IOException;
-import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
-import static java.util.jar.Attributes.Name.IMPLEMENTATION_TITLE;
-import static java.util.jar.Attributes.Name.IMPLEMENTATION_VERSION;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
+@ExtendWith(ManifestParameterResolver.class)
 class AboutEndpointTest {
-  private static final Manifest manifest = mock(Manifest.class);
+  private final ResourceExtension resources;
 
-  private final ResourceExtension resources = ResourceExtension.builder()
-                                                               .addResource(new AboutResource(manifest))
-                                                               .build();
-
-  @BeforeAll
-  static void setUp() {
-    final Attributes attributes = new Attributes();
-    attributes.put(IMPLEMENTATION_VERSION, "1.0");
-    attributes.put(IMPLEMENTATION_TITLE, "about-endpoint-test");
-
-    when(manifest.getMainAttributes()).thenReturn(attributes);
+  private AboutEndpointTest(Manifest manifest) {
+    resources = ResourceExtension.builder().addResource(new AboutResource(manifest)).build();
   }
 
   @Test
