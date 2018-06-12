581c6e8eee82e363015ce8a3e71e927b878ee78a
HDJ <hayco.de.jong@huygens.knaw.nl>
2018-06-12 15:26:24 +0200

    Add start-of-year conversion logic
    TODO: cleanup and adding (corner) test cases

--- src/main/java/nl/knaw/huygens/lobsang/resources/ConversionResource.java
+++ src/main/java/nl/knaw/huygens/lobsang/resources/ConversionResource.java
@@ -1,15 +1,15 @@
 package nl.knaw.huygens.lobsang.resources;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import nl.knaw.huygens.lobsang.api.CalendarPeriod;
 import nl.knaw.huygens.lobsang.api.DateRequest;
 import nl.knaw.huygens.lobsang.api.DateResult;
 import nl.knaw.huygens.lobsang.api.Place;
+import nl.knaw.huygens.lobsang.api.StartOfYear;
 import nl.knaw.huygens.lobsang.api.YearMonthDay;
 import nl.knaw.huygens.lobsang.core.ConversionService;
 import nl.knaw.huygens.lobsang.core.places.PlaceMatcher;
 import nl.knaw.huygens.lobsang.core.places.SearchTermBuilder;
-import org.assertj.core.util.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +20,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import java.time.MonthDay;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -29,11 +33,16 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Comparator.comparing;
 
 @Path("convert")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ConversionResource {
+  private static final MonthDay JANUARY_FIRST = MonthDay.of(1, 1);
+
+  private static final DateTimeFormatter MM_DD = DateTimeFormatter.ofPattern("MM-dd");
+
   private static final Logger LOG = LoggerFactory.getLogger(ConversionResource.class);
 
   private final PlaceMatcher places;
@@ -54,7 +63,7 @@ public class ConversionResource {
   public DateResult convert(@NotNull DateRequest dateRequest) {
     LOG.info("dateRequest: {}", dateRequest);
 
-    final List<Place> candidates = Lists.newArrayList();
+    final List<Place> candidates = new ArrayList<>();
 
     final Map<YearMonthDay, Set<String>> results
       = places.match(termBuilder.build(dateRequest))
@@ -71,7 +80,7 @@ public class ConversionResource {
     final DateResult result;
     if (results.isEmpty()) {
       result = new DateResult(defaultConversion(dateRequest));
-      result.addHint("Requested date lies outside all defined calendar periods, assuming default calendar was in use.");
+      result.addHint("Requested date lies outside all defined calendar periods.");
     } else {
       LOG.debug("results (size {}): {}", results.size(), results);
       result = new DateResult(Lists.newArrayList(results.keySet()));
@@ -95,15 +104,51 @@ public class ConversionResource {
   }
 
   private Function<Place, Stream<YearMonthDay>> convertForPlace(DateRequest dateRequest) {
-    return place -> place.getCalendars().stream()
-                         .map(tryConversion(asYearMonthDay(dateRequest), dateRequest.getTargetCalendar()))
+    final String targetCalendar = dateRequest.getTargetCalendar();
+    final YearMonthDay requestDate = asYearMonthDay(dateRequest);
+    return place -> place.getCalendarPeriods().stream()
+                         .map(calendarPeriod -> conversions.convert(calendarPeriod, requestDate, targetCalendar))
                          .filter(Optional::isPresent)
                          .map(Optional::get)
-                         .peek(it -> it.addNote(String.format("Based on data for place: '%s'", place.getName())));
+                         .map(resultDate -> addPlaceNameNote(resultDate, place))
+                         .map(resultDate -> adjustForYearStart(resultDate, requestDate, place.getStartOfYearList()));
   }
 
-  private Function<CalendarPeriod, Optional<YearMonthDay>> tryConversion(YearMonthDay date, String targetCalendar) {
-    return calendarPeriod -> conversions.convert(calendarPeriod, date, targetCalendar);
+  private YearMonthDay addPlaceNameNote(YearMonthDay result, Place place) {
+    result.addNote(String.format("Based on data for place: '%s'", place.getName()));
+    return result;
+  }
+
+  private YearMonthDay adjustForYearStart(YearMonthDay result, YearMonthDay subject, List<StartOfYear> startOfYears) {
+    final Year subjectYear = Year.of(subject.getYear());
+    return startOfYears.stream()
+                       .filter(startOfYear -> startOfYear.getSince().compareTo(subjectYear) <= 0)
+                       .peek(startOfYear -> LOG.debug("in the race: {}", startOfYear))
+                       .max(comparing(StartOfYear::getSince))
+                       .map(startOfYear -> {
+                         if (!JANUARY_FIRST.equals(startOfYear.getWhen()) && compare(subject, startOfYear) < 0) {
+                           final YearMonthDay adjusted =
+                             new YearMonthDay(result.getYear() + 1, result.getMonth(), result.getDay());
+                           adjusted.setNotes(result.getNotes());
+                           adjusted.addNote(
+                             String.format(
+                               "Date after 1 January, but in this period, new year started on %s, so one year was " +
+                                 "added.",
+                               startOfYear.getWhen().format(MM_DD)));
+                           return adjusted;
+                         } else {
+                           result.addNote("No adjustment necessary based on start of new year.");
+                           return result;
+                         }
+                       })
+                       .orElseGet(() -> {
+                         result.addNote("Insufficient info about when new year started");
+                         return result;
+                       });
+  }
+
+  private int compare(YearMonthDay subject, StartOfYear startOfYear) {
+    return MonthDay.of(subject.getMonth(), subject.getDay()).compareTo(startOfYear.getWhen());
   }
 
   private YearMonthDay defaultConversion(DateRequest dateRequest) {
